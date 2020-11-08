28bfcf80c17bd6cd42e4f384a9a6f8cf0b9e549a
David Arias <davidarias.devs@gmail.com>
2020-11-07 22:08:04 -0500

    :sparkles:Navbar component

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import { motion } from "framer-motion";
+import styles from "./Navbar.module.scss";
+
+const Navbar = ({ links }) => {
+    return (
+        <header className={`${styles.container} flex-row-center padding-x-2`}>
+            <nav className={`${styles.body} flex-row-around`}>
+                {links.map((link, index) => (
+                    <a key={link.href} href="#">
+                        {link.main ? (
+                            <motion.div className={styles.main} whileTap={{ scale: 1.25 }}>
+                                <img src={link.icon} />
+                            </motion.div>
+                        ) : (
+                            <div className={styles.icon}>
+                                <img src={link.icon} className={link.main} />
+                            </div>
+                        )}
+                        <span className={`${styles.name} ${link.main && "margin-top-auto"}`}>
+                            {link.name}
+                        </span>
+                    </a>
+                ))}
+                <svg
+                    width="100%"
+                    height="100%"
+                    viewBox="0 0 100 20"
+                    preserveAspectRatio="xMidYMin slice"
+                    className={styles.background}
+                >
+                    <defs>
+                        <mask id="hole">
+                            <rect width="100%" height="100%" fill="white" />
+                            <circle r="9" cx="50" cy="0" fill="black" />
+                        </mask>
+                    </defs>
+                    <rect x="0" y="0" width="100%" height="100%" fill="white" mask="url(#hole)" />
+                </svg>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 
+//Components
+import Navbar from "../components/Navbar";
+
 //Views
 import Home from "../contents/Home";
 
@@ -9,6 +12,24 @@ export default function Index() {
             <Head>
                 <title>Nuvu Saver</title>
             </Head>
+            <Navbar
+                links={[
+                    { name: "Inicio", href: "home", icon: "/icons/home_24px.svg" },
+                    {
+                        name: "Historial",
+                        href: "history",
+                        icon: "/icons/format_list_bulleted_24px.svg",
+                    },
+                    {
+                        main: true,
+                        name: "Ahorrar",
+                        href: "save",
+                        icon: "/icons/add_24px.svg",
+                    },
+                    { name: "Mi perfil", href: "profile", icon: "/icons/account_circle_24px.svg" },
+                    { name: "Más", href: "more", icon: "/icons/more_horiz_24px.svg" },
+                ]}
+            />
             <Home />
         </div>
     );
